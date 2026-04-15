4a2f3de1168c29f6e8eea85c49703ae368c34b6e
Gusnand <gusnanda1170@gmail.com>
2026-04-15 14:32:49 +0800

    Initial Commit

---
+++ photoremake-rn/photoremake_rnApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoremake_rnApp.swift
+//  photoremake-rn
+//
+//  Created by Ketut Agus Cahyadi Nanda on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct photoremake_rnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
